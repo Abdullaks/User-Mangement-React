66359b621c57a3d919fcffc3440acf8840bcb5a7
Abdullaks <abdullaks1999@gmail.com>
2022-09-24 17:56:03 +0530

    first commit

---
+++ backend/models/userModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+
+
+const userSchema = mongoose.Schema({
+
+    name: {
+        type: String,
+        required: [true, 'please Add a name'],
+    },
+    email: {
+        type: String,
+        required: [true, 'please Add an email'],
+        unique: true,
+    },
+    mobile: {
+        type: String,
+        required: [true, 'please Add a phone number'],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'please Add a password'],
+    },
+    role: {
+        type: String,
+        enuum: ["user", "admin"],
+        default: "user"
+    }
+},
+    {
+        timestamps: true,
+
+    }
+)
+
+
+userSchema.pre("save", async function (next) {
+
+    if (this.email == 'admin123@gmail.com') {
+        this.role = "admin"
+
+    }
+    next();
+})
+
+
+
+
+module.exports = mongoose.model('User', userSchema)
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const router=express.Router()
+const {registerUser, loginUser, getMe,findOneuser,editUser}=require('../controllers/userController')
+const {protect}=require('../middleware/authMiddleware')
+
+
+
+router.post('/',registerUser)
+router.post('/login',loginUser)
+router.get('/userData',protect,getMe)
+router.get('/getOneUser/:id',findOneuser)
+router.post('/editUser/:id',editUser)
+
+ 
+
+module.exports=router
